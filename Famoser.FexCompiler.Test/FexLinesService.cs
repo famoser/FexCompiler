9324445d9ef710480a2aca617b61758418b2ebf8
Florian Moser <products@famoser.ch>
2018-01-26 19:21:07 +0100

    better fexLine test

--- Famoser.FexCompiler.Test/FexLinesService.cs
+++ Famoser.FexCompiler.Test/FexLinesService.cs
@@ -1,4 +1,5 @@
-﻿using Famoser.FexCompiler.Services;
+﻿using System.Collections.Generic;
+using Famoser.FexCompiler.Services;
 using Famoser.FexCompiler.Test.Helpers;
 using Famoser.FexCompiler.Test.Service.Base;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -12,6 +13,49 @@ namespace Famoser.FexCompiler.Test
         {
             //arrange
             var fileService = new FileService(TestHelper.GetInputFilePath(fileName));
+            var lines = fileService.Process();
+            var fexService = new FexService(lines);
+
+            //act
+            var fexLines = fexService.Process();
+
+            //assert 
+            Assert.IsTrue(fexLines.Count > 0);
+
+            var dic = new Dictionary<string, int>()
+            {
+                {"simple.fex", 4},
+                {"advanced.fex", 11}
+            };
+            Assert.AreEqual(fexLines.Count, dic[fileName]);
+
+            var dic2 = new Dictionary<string, List<int>>()
+            {
+                {
+                    "simple.fex",
+                    new List<int>()
+                    {
+                        0,1,2,2
+                    }
+                },
+                {"advanced.fex",
+                    new List<int>()
+                    {
+                        0,1,2,3,3,2,2,3,4,4,2
+                    }
+                }
+            };
+            for (int i = 0; i < dic2[fileName].Count; i++)
+            {
+                Assert.AreEqual(fexLines[i].Level, dic2[fileName][i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestSimpleFexLine()
+        {
+            //arrange
+            var fileService = new FileService(TestHelper.GetInputFilePath("simple.fex"));
             var lines = fileService.Process();
             var fexService = new FexService(lines);
 
