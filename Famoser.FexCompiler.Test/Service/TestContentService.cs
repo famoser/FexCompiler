af0c08950b91e9560f393141ad16109ef7ce22e2
Florian Moser <products@famoser.ch>
2018-01-26 20:34:08 +0100

    more content service testing

--- Famoser.FexCompiler.Test/Service/TestContentService.cs
+++ Famoser.FexCompiler.Test/Service/TestContentService.cs
@@ -48,7 +48,7 @@ namespace Famoser.FexCompiler.Test.Service
 
             //act
             var content = contentService.Process();
-            
+
             //assert 
             Assert.IsTrue(content.Content[0] is Section);
             var section1 = (Section)content.Content[0];
@@ -59,13 +59,71 @@ namespace Famoser.FexCompiler.Test.Service
             Assert.AreEqual(section1.Content.Count, 1);
             Assert.IsTrue(section1.Content[0] is Section);
 
-            var section2 = (Section) section1.Content[0];
+            var section2 = (Section)section1.Content[0];
             Assert.AreEqual(section2.Header.TextNodes.Count, 1);
             Assert.AreEqual(section2.Header.TextNodes[0].Text, "H2");
             Assert.AreEqual(section2.TextContent[0].TextNodes.Count, 1);
             Assert.AreEqual(section2.TextContent[0].TextNodes[0].Text, "hallo");
             Assert.AreEqual(section2.TextContent[1].TextNodes.Count, 1);
             Assert.AreEqual(section2.TextContent[1].TextNodes[0].Text, "welt");
+        }
+
+        [TestMethod]
+        public void TestLongFexLine()
+        {
+            //arrange
+            var fileService = new FileService(TestHelper.GetInputFilePath("long.fex"));
+            var lines = fileService.Process();
+            var fexService = new FexService(lines);
+            var fexLines = fexService.Process();
+            var contentService = new ContentService(fexLines);
+
+            //act
+            var content = contentService.Process();
+
+            //assert 
+            Assert.IsTrue(content.Content[0] is Section);
+            Assert.IsTrue(content.Content[1] is Section);
+            var section1 = (Section)content.Content[0];
+            var section4 = (Section)content.Content[1];
+
+            Assert.AreEqual(section1.Header.TextNodes.Count, 1);
+            Assert.AreEqual(section1.Header.TextNodes[0].Text, "H1");
+            Assert.AreEqual(section1.TextContent.Count, 0);
+            Assert.AreEqual(section1.Content.Count, 2);
+            Assert.IsTrue(section1.Content[0] is Section);
+            Assert.IsTrue(section1.Content[1] is Section);
+
+            var section2 = (Section)section1.Content[0];
+            Assert.AreEqual(section2.Header.TextNodes.Count, 1);
+            Assert.AreEqual(section2.Header.TextNodes[0].Text, "H2");
+            Assert.AreEqual(section2.TextContent.Count, 0);
+            Assert.AreEqual(section2.Content.Count, 1);
+            Assert.IsTrue(section2.Content[0] is Section);
+            
+            var section21 = (Section) section2.Content[0];
+            Assert.AreEqual(section21.Header.TextNodes.Count, 1);
+            Assert.AreEqual(section21.Header.TextNodes[0].Text, "H2.1");
+            Assert.AreEqual(section21.TextContent.Count, 2); //skip textContent inspection
+            Assert.AreEqual(section21.Content.Count, 0);
+
+            var section3 = (Section)section1.Content[1];
+            Assert.AreEqual(section3.Header.TextNodes.Count, 1);
+            Assert.AreEqual(section3.Header.TextNodes[0].Text, "H3");
+            Assert.AreEqual(section3.TextContent.Count, 2);
+            Assert.AreEqual(section3.Content.Count, 0);
+            
+            Assert.AreEqual(section4.Header.TextNodes.Count, 1);
+            Assert.AreEqual(section4.Header.TextNodes[0].Text, "H4");
+            Assert.AreEqual(section4.TextContent.Count, 0);
+            Assert.AreEqual(section4.Content.Count, 1);
+            Assert.IsTrue(section4.Content[0] is Section);
+
+            var section5 = (Section)section4.Content[0];
+            Assert.AreEqual(section5.Header.TextNodes.Count, 1);
+            Assert.AreEqual(section5.Header.TextNodes[0].Text, "H5");
+            Assert.AreEqual(section5.TextContent.Count, 2);
+            Assert.AreEqual(section5.Content.Count, 0);
         }
     }
 }
