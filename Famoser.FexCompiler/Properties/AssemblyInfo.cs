745bfb7146a3f933ab14c91b4067cf4c3d078d5a
Florian Moser <git@famoser.ch>
2021-03-30 15:20:17 +0200

    Update assembly info

--- Famoser.FexCompiler/Properties/AssemblyInfo.cs
+++ Famoser.FexCompiler/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FexCompiler")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Compile .fex files to printable .pdf ")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("famoser")]
 [assembly: AssemblyProduct("FexCompiler")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
